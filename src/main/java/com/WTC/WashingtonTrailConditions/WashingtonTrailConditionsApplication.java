1014defbe53e9f420994196067b9240e1732631b
Jackson Hayward <jchayward1@gmail.com>
2022-10-12 11:25:55 -0700

    reorganize database

--- src/main/java/com/WTC/WashingtonTrailConditions/WashingtonTrailConditionsApplication.java
+++ src/main/java/com/WTC/WashingtonTrailConditions/WashingtonTrailConditionsApplication.java
@@ -20,7 +20,29 @@ public class WashingtonTrailConditionsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Running app");
+		// used to read data from csv and save to database
+		/*Scanner sc = new Scanner(new File("hikeData.csv"));
+		sc.useDelimiter("\n");
+		int i = 0;
+		while (sc.hasNext()) {
+			String data = sc.next();
+			String[] trailData = data.split(",");
+
+			String name = trailData[0];
+			String region = trailData[1];
+			String image = trailData[2];
+			String lat = trailData[3];
+			String lon = trailData[4];
+			String link = trailData[5];
+			i++;
+			System.out.println(link);
+			System.out.println(i);
+
+			Trail toAdd = new Trail(name, region, image, lat, lon, link);
+			// trailRepository.save(toAdd);
+		}
+		sc.close();*/
+
 	}
 
 }
